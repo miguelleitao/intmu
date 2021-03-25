8f168bb817f8ac61afd4e6bb7947f4831ff54bd7
Miguel Leitao <jml@isep.ipp.pt>
2021-03-25 10:20:43 +0000

    simulador

---
+++ docs/lab2/dash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Dash screen simulator
+# Miguel Leitao, ISEP
+# v0.21, mar-2020
+bname=dash_temp_img
+tmpdir=.
+bpath=$tmpdir/$bname
+dist_table='1,1 268,122  1,599 257,409  799,599 780,406  799,1 770,130'
+if [ ! -f "$1" ]; then
+	echo "Usage: $0 image"
+	exit;
+fi
+if [ ! -r dash.jpg ]; then
+	wget -q www.dee.isep.ipp.pt/~jml/intmu/lab2/dash.jpg -O dash.jpg
+fi
+convert $1 -gravity center -crop 800x600+0+0 +repage ${bpath}_c.png
+convert ${bpath}_c.png -matte -virtual-pixel transparent -distort Perspective "$dist_table" ${bpath}_d.png
+convert -composite dash.jpg ${bpath}_d.png out.png
+display out.png 2>/dev/null
+rm -f ${bpath}_?.png
+
