00641ad6fe87482880a6ee2b79c9aaebbb98c143
Miguel Leitao <jml@isep.ipp.pt>
2021-03-26 16:23:20 +0000

    Repo url

--- docs/lab2/dash.sh
+++ docs/lab2/dash.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Dash screen simulator
 # Miguel Leitao, ISEP
-# v0.21, mar-2020
+# v0.22, mar-2021
 bname=dash_temp_img
 tmpdir=.
 bpath=$tmpdir/$bname
+repo=https://miguelleitao.gitlab.io/intmu/lab2
 dist_table='1,1 268,122  1,599 257,409  799,599 780,406  799,1 770,130'
 if [ ! -f "$1" ]; then
 	echo "Usage: $0 image"
 	exit;
 fi
 if [ ! -r dash.jpg ]; then
-	wget -q www.dee.isep.ipp.pt/~jml/intmu/lab2/dash.jpg -O dash.jpg
+	wget -q $repo/dash.jpg -O dash.jpg
 fi
 convert $1 -gravity center -crop 800x600+0+0 +repage ${bpath}_c.png
 convert ${bpath}_c.png -matte -virtual-pixel transparent -distort Perspective "$dist_table" ${bpath}_d.png
